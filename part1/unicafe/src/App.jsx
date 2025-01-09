bae20a218d0dd11d08ccc61aaebce85205634bb4
Dan Murphy <dan.murphy@alumni.utoronto.ca>
2025-01-09 09:15:50 -0500

    feat(unicafe): complete exercise 1.6

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,11 +1,30 @@
+import { useState } from 'react'
+
+function Button({ handleClick, text }) {
+  return <button onClick={handleClick}>{text}</button>
+}
+
 function App() {
+  const [good, setGood] = useState(0)
+  const [bad, setBad] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+
   return (
     <>
       <section>
         <h1>Give Feedback</h1>
+        <Button handleClick={() => setGood(good + 1)} text='good' />
+        <Button handleClick={() => setBad(bad + 1)} text='bad' />
+        <Button handleClick={() => setNeutral(neutral + 1)} text='neutral' />
       </section>
+
       <section>
         <h1>Statistics</h1>
+        <p>
+          good: {good} <br />
+          bad: {bad} <br />
+          neutral: {neutral}
+        </p>
       </section>
     </>
   )
