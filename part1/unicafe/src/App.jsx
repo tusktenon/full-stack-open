ef38bc55d19d83bcec1c817cb0b74304c06733f2
Dan Murphy <dan.murphy@alumni.utoronto.ca>
2025-01-09 08:37:38 -0500

    feat: add unicafe application

---
+++ part1/unicafe/src/App.jsx
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <>
+      <section>
+        <h1>Give Feedback</h1>
+      </section>
+      <section>
+        <h1>Statistics</h1>
+      </section>
+    </>
+  )
+}
+
+export default App
