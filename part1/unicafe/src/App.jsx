6e97d12c5c31a94780ed8954f691e7f90473e616
Dan Murphy <dan.murphy@alumni.utoronto.ca>
2025-01-09 09:34:50 -0500

    feat(unicafe): complete exercise 1.7
    Show more statistics: total, average, percent positive.

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -6,24 +6,31 @@ function Button({ handleClick, text }) {
 
 function App() {
   const [good, setGood] = useState(0)
-  const [bad, setBad] = useState(0)
   const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const all = good + neutral + bad
+  const average = all && (good - bad) / all
+  const positive = all && 100 * (good / all)
 
   return (
     <>
       <section>
         <h1>Give Feedback</h1>
         <Button handleClick={() => setGood(good + 1)} text='good' />
-        <Button handleClick={() => setBad(bad + 1)} text='bad' />
         <Button handleClick={() => setNeutral(neutral + 1)} text='neutral' />
+        <Button handleClick={() => setBad(bad + 1)} text='bad' />
       </section>
 
       <section>
         <h1>Statistics</h1>
         <p>
           good: {good} <br />
+          neutral: {neutral} <br />
           bad: {bad} <br />
-          neutral: {neutral}
+          all: {all} <br />
+          average: {average.toFixed(3)} <br />
+          positive: {positive.toFixed(1)} %
         </p>
       </section>
     </>
