7635cb1e5d63aede5a090b0f157e328eb3825434
Dan Murphy <dan.murphy@alumni.utoronto.ca>
2025-01-10 16:02:55 -0500

    feat(phonebook): complete exercise 2.9
    Add a search field that filters the list of entries by name.

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,9 +1,23 @@
 import { useState } from 'react'
 
 function App() {
-  const [persons, setPersons] = useState([{ name: 'Arto Hellas', number: '040-1234567'}])
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 },
+  ])
+
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+  const [search, setSearch] = useState('')
+
+  const personsToShow =
+    search.length === 0
+      ? persons
+      : persons.filter(person =>
+          person.name.toLowerCase().includes(search.toLowerCase()),
+        )
 
   const addPerson = event => {
     event.preventDefault()
@@ -13,7 +27,8 @@ function App() {
       setPersons(
         persons.concat({
           name: newName,
-          number: newNumber
+          number: newNumber,
+          id: persons.length + 1,
         }),
       )
     }
@@ -29,9 +44,17 @@ function App() {
     setNewNumber(event.target.value)
   }
 
+  const handleSearchChange = event => {
+    setSearch(event.target.value)
+  }
+
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        filter shown with <input value={search} onChange={handleSearchChange} />
+      </div>
+      <h3>Add new entry</h3>
       <form onSubmit={addPerson}>
         <div>
           name: <input value={newName} onChange={handleNameChange} />
@@ -43,10 +66,12 @@ function App() {
           <button type='submit'>add</button>
         </div>
       </form>
-      <h2>Numbers</h2>
+      <h3>Numbers</h3>
       <ul>
-        {persons.map(person => (
-          <li key={person.name}>{person.name} {person.number}</li>
+        {personsToShow.map(person => (
+          <li key={person.id}>
+            {person.name} {person.number}
+          </li>
         ))}
       </ul>
     </div>
