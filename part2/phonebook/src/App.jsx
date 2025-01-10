af65c8e20a5ad0a1a59971d9fad20f8d5cb0161d
Dan Murphy <dan.murphy@alumni.utoronto.ca>
2025-01-10 15:43:53 -0500

    feat(phonebook): complete exercise 2.8
    Add phone numbers.

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,8 +1,9 @@
 import { useState } from 'react'
 
 function App() {
-  const [persons, setPersons] = useState([{ name: 'Arto Hellas' }])
+  const [persons, setPersons] = useState([{ name: 'Arto Hellas', number: '040-1234567'}])
   const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
 
   const addPerson = event => {
     event.preventDefault()
@@ -12,14 +13,20 @@ function App() {
       setPersons(
         persons.concat({
           name: newName,
+          number: newNumber
         }),
       )
     }
     setNewName('')
+    setNewNumber('')
   }
 
   const handleNameChange = event => {
     setNewName(event.target.value)
+  }
+
+  const handleNumberChange = event => {
+    setNewNumber(event.target.value)
   }
 
   return (
@@ -30,13 +37,16 @@ function App() {
           name: <input value={newName} onChange={handleNameChange} />
         </div>
         <div>
+          number: <input value={newNumber} onChange={handleNumberChange} />
+        </div>
+        <div>
           <button type='submit'>add</button>
         </div>
       </form>
       <h2>Numbers</h2>
       <ul>
         {persons.map(person => (
-          <li key={person.name}>{person.name}</li>
+          <li key={person.name}>{person.name} {person.number}</li>
         ))}
       </ul>
     </div>
