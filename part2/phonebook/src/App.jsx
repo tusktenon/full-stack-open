5220dd1c4ef427ee895c023727b2a1dfbcc59b6a
Dan Murphy <dan.murphy@alumni.utoronto.ca>
2025-01-12 22:40:59 -0500

    feat(phonebook): complete exercise 2.16
    Display a notification after a successful operation (add, update or
    delete).

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react'
 import Filter from './components/Filter'
+import Notification from './components/Notification'
 import PersonForm from './components/PersonForm'
 import Persons from './components/Persons'
 import entryService from './services/entries'
@@ -9,6 +10,7 @@ function App() {
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [search, setSearch] = useState('')
+  const [notification, setNotification] = useState(null)
 
   useEffect(() => {
     entryService.getAll().then(initialEntries => setPersons(initialEntries))
@@ -18,16 +20,22 @@ function App() {
     event.preventDefault()
     const existing = persons.find(person => person.name === newName)
     if (existing) {
-      const message = `${existing.name} is already in the phonebook. Replace the old number with a new one?`
+      const message = `${newName} is already in the phonebook. Replace the old number with a new one?`
       if (confirm(message)) {
         entryService.update(existing, newNumber).then(updated => {
           setPersons(persons.map(p => (p.id === existing.id ? updated : p)))
+          setNotification(`Updated number for ${newName}`)
+          setTimeout(() => setNotification(null), 3000)
         })
       }
     } else {
       entryService
         .create({ name: newName, number: newNumber })
-        .then(newEntry => setPersons(persons.concat(newEntry)))
+        .then(newEntry => {
+          setPersons(persons.concat(newEntry))
+          setNotification(`Added ${newName}`)
+          setTimeout(() => setNotification(null), 3000)
+        })
     }
     setNewName('')
     setNewNumber('')
@@ -47,15 +55,18 @@ function App() {
 
   const handleRemovalOf = person => {
     if (confirm(`Delete ${person.name} ?`)) {
-      entryService
-        .remove(person)
-        .then(() => setPersons(persons.filter(p => p.id !== person.id)))
+      entryService.remove(person).then(() => {
+        setPersons(persons.filter(p => p.id !== person.id))
+        setNotification(`Removed ${person.name}`)
+        setTimeout(() => setNotification(null), 3000)
+      })
     }
   }
 
   return (
     <div>
       <h2>Phonebook</h2>
+      <Notification message={notification} />
       <Filter search={search} onChange={handleSearchChange} />
 
       <h3>Add new entry</h3>
