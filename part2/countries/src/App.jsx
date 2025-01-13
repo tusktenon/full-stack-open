d8522d99f0e19bffe5f205baefd66f46506aba43
Dan Murphy <dan.murphy@alumni.utoronto.ca>
2025-01-13 11:41:48 -0500

    feat(countries): complete exercise 2.18

--- part2/countries/src/App.jsx
+++ part2/countries/src/App.jsx
@@ -1,11 +1,77 @@
-import { useState } from 'react'
+import axios from 'axios'
+import { useEffect, useState } from 'react'
+
+function Results({ countries, search }) {
+  const results = countries
+    .map((c, i) => ({ id: i, name: c.name.common }))
+    .filter(country =>
+      country.name.toLowerCase().includes(search.toLowerCase()),
+    )
+
+  if (results.length > 10) {
+    return <p>Too many matches, specify another filter.</p>
+  }
+
+  if (results.length > 1) {
+    return (
+      <>
+        {results.map(c => (
+          <p key={c.id}>{c.name}</p>
+        ))}
+      </>
+    )
+  }
+
+  if (results.length == 1) {
+    return <Country country={countries[results[0].id]} />
+  }
+
+  return <p>No matches found.</p>
+}
+
+function Country({ country }) {
+  console.log(country)
+  return (
+    <>
+      <h2>{country.name.common}</h2>
+      <p>Capital: {country.capital[0]}</p>
+      <p>Area: {country.area}</p>
+
+      <h3>Languages:</h3>
+      <ul>
+        {Object.values(country.languages).map(lang => (
+          <li>{lang}</li>
+        ))}
+      </ul>
+
+      <img src={country.flags.png} alt={country.flags.alt} />
+    </>
+  )
+}
 
 function App() {
+  const [countries, setCountries] = useState([])
   const [search, setSearch] = useState('')
+
+  const baseURL = 'https://studies.cs.helsinki.fi/restcountries/api/'
+
+  useEffect(() => {
+    axios.get(`${baseURL}\all`).then(response => {
+      const sorted = response.data.toSorted((a, b) =>
+        a.name.common.localeCompare(b.name.common),
+      )
+      setCountries(sorted)
+    })
+  }, [])
+
+  const handleSearchChange = event => setSearch(event.target.value)
 
   return (
     <>
-      <p>The countries application</p>
+      <div>
+        find countries: <input value={search} onChange={handleSearchChange} />
+      </div>
+      <Results countries={countries} search={search} />
     </>
   )
 }
