d7dccfa68356e461663edfcb38360df742216fd3
Dan Murphy <dan.murphy@alumni.utoronto.ca>
2025-01-13 09:56:00 -0500

    feat: add countries application

---
+++ part2/countries/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+function App() {
+  const [search, setSearch] = useState('')
+
+  return (
+    <>
+      <p>The countries application</p>
+    </>
+  )
+}
+
+export default App
