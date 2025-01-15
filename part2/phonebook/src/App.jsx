6494e0116319835ab76197f37e2f353e38c80ab1
Dan Murphy <dan.murphy@alumni.utoronto.ca>
2025-01-14 19:50:20 -0500

    refactor(phonebook): extract functions

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -16,43 +16,6 @@ function App() {
     entryService.getAll().then(initialEntries => setPersons(initialEntries))
   }, [])
 
-  const displayNotification = (message, style = 'info') => {
-    setNotification({ message, style })
-    setTimeout(() => setNotification(null), 3000)
-  }
-
-  const addPerson = event => {
-    event.preventDefault()
-    const existing = persons.find(person => person.name === newName)
-    if (existing) {
-      const message = `${newName} is already in the phonebook. Replace the old number with a new one?`
-      if (confirm(message)) {
-        entryService
-          .update(existing, newNumber)
-          .then(updated => {
-            setPersons(persons.map(p => (p.id === existing.id ? updated : p)))
-            displayNotification(`Updated number for ${newName}`)
-          })
-          .catch(() => {
-            displayNotification(
-              `Entry for ${newName} has already been removed from the server`,
-              'error',
-            )
-            setPersons(persons.filter(p => p.id !== existing.id))
-          })
-      }
-    } else {
-      entryService
-        .create({ name: newName, number: newNumber })
-        .then(newEntry => {
-          setPersons(persons.concat(newEntry))
-          displayNotification(`Added ${newName}`)
-        })
-    }
-    setNewName('')
-    setNewNumber('')
-  }
-
   const handleNameChange = event => {
     setNewName(event.target.value)
   }
@@ -65,13 +28,56 @@ function App() {
     setSearch(event.target.value)
   }
 
-  const handleRemovalOf = person => {
+  const displayNotification = (message, style = 'info') => {
+    setNotification({ message, style })
+    setTimeout(() => setNotification(null), 3000)
+  }
+
+  const addPerson = () => {
+    entryService.create({ name: newName, number: newNumber }).then(newEntry => {
+      setPersons(persons.concat(newEntry))
+      displayNotification(`Added ${newName}`)
+    })
+  }
+
+  const updatePerson = person => {
+    const message = `${newName} is already in the phonebook. Replace the old number with a new one?`
+    if (confirm(message)) {
+      entryService
+        .update(person, newNumber)
+        .then(updated => {
+          setPersons(persons.map(p => (p.id === person.id ? updated : p)))
+          displayNotification(`Updated number for ${newName}`)
+        })
+        .catch(() => {
+          displayNotification(
+            `Entry for ${newName} has already been removed from the server`,
+            'error',
+          )
+          setPersons(persons.filter(p => p.id !== person.id))
+        })
+    }
+  }
+
+  const removePerson = person => {
     if (confirm(`Delete ${person.name} ?`)) {
       entryService.remove(person).then(() => {
         setPersons(persons.filter(p => p.id !== person.id))
         displayNotification(`Removed ${person.name}`)
       })
     }
+  }
+
+  const handleFormSubmission = event => {
+    event.preventDefault()
+    const existing = persons.find(person => person.name === newName)
+    if (existing) {
+      updatePerson(existing)
+    } else {
+      addPerson()
+    }
+    setNewName('')
+    setNewNumber('')
   }
 
   return (
@@ -89,11 +95,11 @@ function App() {
         onNameChange={handleNameChange}
         number={newNumber}
         onNumberChange={handleNumberChange}
-        onSubmit={addPerson}
+        onSubmit={handleFormSubmission}
       />
 
       <h3>Numbers</h3>
-      <Persons persons={persons} search={search} onRemove={handleRemovalOf} />
+      <Persons persons={persons} search={search} onRemove={removePerson} />
     </div>
   )
 }
